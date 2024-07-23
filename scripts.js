12ed1adc6350d0ea8a2a88d624553463f262f231
Madan K C <madankc71@gmail.com>
2024-07-23 12:14:48 -0400

    Added skill section and update content and css

--- scripts.js
+++ scripts.js
@@ -1,16 +1,21 @@
-document.querySelectorAll('nav ul li a').forEach(link => {
-    link.addEventListener('click', function() {
-        document.querySelectorAll('nav ul li a').forEach(navLink => navLink.classList.remove('active'));
-        this.classList.add('active');
-    });
-});
-
-
 // Function to toggle navigation menu visibility
 function toggleMenu() {
     const nav = document.querySelector('nav ul');
     nav.classList.toggle('active');
 }
 
+// Function to close the menu
+function closeMenu() {
+    const nav = document.querySelector('nav ul');
+    if (nav.classList.contains('active')) {
+        nav.classList.remove('active');
+    }
+}
+
 // Event listener for toggle button
-document.querySelector('.toggle-btn').addEventListener('click', toggleMenu);
+document.querySelector('.toggle-btn').addEventListener('click', toggleMenu); 
+
+// Event listener for menu items to close the menu when an item is clicked
+document.querySelectorAll('nav ul li a').forEach(item => {
+    item.addEventListener('click', closeMenu);
+});
